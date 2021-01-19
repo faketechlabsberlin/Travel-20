34c1829554689bcf47521443a4009c6fbe413a87
nataliedieckmann <natalie.dieckmn@gmail.com>
2021-01-19 19:41:01 +0100

    hooked up new covid data to dropdown lists

--- Dev/app.js
+++ Dev/app.js
@@ -1,30 +1,13 @@
-const countriesListURL = 'https://api.jsonbin.io/b/5fff60faf98f6e35d5fc2daa'; // This is on some json hosting site
-const countriesDataURL = 'https://api.jsonbin.io/b/6002f41bf98f6e35d5fd39af'; // This is on some json hosting site
+const covidDataURL = 'https://api.jsonbin.io/b/60072108eb2fee239b5ee9ab'; // This is on some json hosting site
 
-let countriesList = {};
 let countriesData = {}; 
 
-// Get countries list
-// request('GET', countriesListURL, true) // this is calling the request function which returns a promise of XMLHttpRequest. We use GET because we are retreiving a JSON (REST API)
-//   .then(function (e) { // .then is done on resolve (successful connection)
-//       const json = e.target.response; // get the string from the response
-//       countriesList = JSON.parse(json).countries; // convert it to an object
-//       // console.log(countriesList);
-//       // populateCountriesList(countriesList);
-//   })
-//   .catch( function (e) { // this is called on reject when an error happens on the connection
-//     console.log("Failed to retreive countries.json", e); 
-//     document.querySelector('#error').innerHTML= e;
-//     document.getElementById("explore").setAttribute("class", "hidden");
-//     document.getElementById("page404").setAttribute("class", "container resultField");
-//   });
-
 // Get countries data
-request('GET', countriesDataURL, true) // this is calling the request function which returns a promise of XMLHttpRequest. We use GET because we are retreiving a JSON (REST API)
+request('GET', covidDataURL, true) // this is calling the request function which returns a promise of XMLHttpRequest. We use GET because we are retreiving a JSON (REST API)
   .then(function (e) { // .then is done on resolve (successful connection)
       const json = e.target.response; // get the string from the response
-      const data = JSON.parse(json).countriesData; // convert it to an object
-
+      const data = JSON.parse(json); // convert it to an object
+   
       for (var i = 0, countryData; i < data.length; i++) {
         countryData = data[i];
         countriesData[ countryData.name ] = countryData;
@@ -51,46 +34,24 @@ function request(method, url) { // this function wraps a XMLHttpRequest into a p
 }
   
 function populateCountriesList(countriesData){
+
+  setCountryDropdownElement("#currentDestination", "Germany");
+
   //populate drop down - current location with countriesData object
   for (let country in countriesData){
 
-    const currentDest = document.querySelector('#currentDestination');
-    let countryElem = document.createElement('option');
-    countryElem.innerHTML = country;
-    if(country !== 'Germany'){
-          countryElem.setAttribute('disabled', true);
-        }
-    currentDest.appendChild(countryElem);
-
     //populate drop down - destination with countriesData object
-  
-    const plannedDest = document.querySelector('#plannedDestination');
-    let countryElem2 = document.createElement('option');
-    countryElem2.innerHTML = country;
-    plannedDest.appendChild(countryElem2);
+    setCountryDropdownElement("#plannedDestination", country);
   }
 }
-  
-  // countries.map((country)=>{
-  //   const currentDest = document.querySelector('#currentDestination');
-  //   let countryElem = document.createElement('option');
-  //   countryElem.innerHTML = country;
-    
-  //   if(country !== 'Germany'){
-  //       countryElem.setAttribute('disabled', true);
-  //   }
-    
-  //   currentDest.appendChild(countryElem);
-  // })
 
-  //populate drop down - destination with a country list
-  // countries.map((country)=>{
-  //   const plannedDest = document.querySelector('#plannedDestination');
-  //   let countryElem = document.createElement('option');
-  //   countryElem.innerHTML = country;
-  //   plannedDest.appendChild(countryElem);
-  // })
-
+function setCountryDropdownElement(dropdownTag, countryName)
+{
+  const dropdownElement = document.querySelector(dropdownTag);
+  let countryElement = document.createElement('option');
+  countryElement.innerHTML = countryName;
+  dropdownElement.appendChild(countryElement);
+}
 
 //Germany selected automatically when drop-down clicked (temporary)
 const currentDest = document.querySelector('#currentDestination');
@@ -134,6 +95,9 @@ function onGoButtonClicked() {
   // Disable explore frame and show result frame
   document.getElementById("explore").setAttribute("class", "hidden");
   document.getElementById("result").setAttribute("class", "container resultField");
+
+  const fromTitle = document.querySelector('#fromTitle');
+  fromTitle.innerHTML = `From: ${currentDest} to ${plannedDest}`;
 }
 
 function UpdateCurrentDestinationElements(countryName, countryData)
@@ -165,7 +129,9 @@ function UpdatePlannedDestinationElements(countryName, countryData)
       return;
   }
 
-  plannedDestData();
+  //plannedDestData();
+
+  /*
   // Hook up rest of elements using data from countryData
   function plannedDestData(){
     const properties = Object.keys(countryData);
@@ -188,7 +154,7 @@ function UpdatePlannedDestinationElements(countryName, countryData)
         }
       }
     }
-  }
+  }*/
 }
   
 
